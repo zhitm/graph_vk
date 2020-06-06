0e91f2e36bececea5f820baaddfa3429123b1b51
zhitm <64278750+zhitm@users.noreply.github.com>
2020-06-06 11:49:38 +0300

    del of node+no withpout friends

---
+++ experiment.py
@@ -0,0 +1,7 @@
+class Class:
+	def __init__(self,x):
+		self.x = x
+f = Class(1)
+s = Class(1)
+print(s == f)
+
--- graph.py
+++ graph.py
@@ -12,6 +12,14 @@ class Graph:
 		self.graph.update({node: node.friends})
 		self.node_cnt += 1
 		Node.id_node_dict.update({id: node})
+
+	def del_node(self, node):
+		if node in self.nodes:
+			self.nodes.remove(node)
+		for friend in node.friends:
+			friend.friends.discard(node)
+			self.graph.update({friend: friend.friends})
+		self.graph.pop(node)
 	def add_edge(self, node1, node2):
 		node1.friends.add(node2)
 		node2.friends.add(node1)
@@ -51,11 +59,14 @@ if __name__ == '__main__':
 		line = line.strip()
 		arr = line.split()
 		id = arr[0]
-		g.add_node(id)
-		node = Node.id_to_node(id)
-		for friend_id in arr[1:]:
+		if arr[1:] != None:
+			g.add_node(id)
 			node = Node.id_to_node(id)
-			friend = Node.id_to_node(friend_id)
-			friend = Node.id_to_node(friend_id)
-			g.add_edge(node, friend)
-	print("ok")
+			for friend_id in arr[1:]:
+				node = Node.id_to_node(id)
+				friend = Node.id_to_node(friend_id)
+				g.add_edge(node, friend)
+		else:
+			pass
+	print('ok')
+
