0bae9223e3e3edb920a9e8174c307d60813a778d
Kremator1007 <64495509+Kremator1007@users.noreply.github.com>
2020-06-08 22:41:27 +0300

    Fixed indentation

--- vk_get_names.py
+++ vk_get_names.py
@@ -10,10 +10,10 @@ token = '5641c5e45641c5e45641c5e4f25633ab13556415641c5e4089bd500489a896d0d89892d
 ids = [int(line.strip()) for line in file1]
 
 for k in range(len(ids)):
-    user_id = ids[k]
-    url = f'https://api.vk.com/method/users.get?v=5.107&user_id={user_id}&access_token={token}'
-    data = json.loads(urlopen(url).read())
-    
+	user_id = ids[k]
+	url = f'https://api.vk.com/method/users.get?v=5.107&user_id={user_id}&access_token={token}'
+	data = json.loads(urlopen(url).read())
+	
 	try:
 		file2.write(str(user_id) + data['response'][0]['first_name'] + data['response'][0]['last_name'] + '\n')
 	
